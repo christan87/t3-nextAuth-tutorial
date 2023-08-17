449a4e00fd0eb6992f294d6f308e01bd5a4121ed
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-08-17 07:05:05 -0700

    added signin button for no user detected

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,7 +71,14 @@ export default function Home() {
   api.posts.getAll.useQuery();
   const user = session?.user;
 
-  if(!user) return <div />
+  if(!user) return <div className="flex justify-center items-center h-screen">
+    <button 
+      className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+      onClick={() => void signIn()}
+    >
+      Sign In
+    </button>
+  </div>
 
   return (
     <>
@@ -85,12 +92,12 @@ export default function Home() {
           <div className="flex border-b border-slate-400 p-4">
             <div className="flex justify-center w-full">
               {session && <CreatePostWizard />}
-              <button
+              {/*<button
                 className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
                 onClick={session? () => void signOut() : () => void signIn()}
               >
                 {session ? "Sign out" : "Sign in"}
-              </button>
+              </button>*/}
             </div>
           </div>
           <Feed />
