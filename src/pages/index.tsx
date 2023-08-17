5c588b3854f9a5e002721ce4ff11e44b89cc5a9e
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-08-17 06:40:36 -0700

    added sigin button back

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -85,12 +85,12 @@ export default function Home() {
           <div className="flex border-b border-slate-400 p-4">
             <div className="flex justify-center w-full">
               {session && <CreatePostWizard />}
-              {/*<button
+              <button
                 className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
                 onClick={session? () => void signOut() : () => void signIn()}
               >
                 {session ? "Sign out" : "Sign in"}
-              </button>*/}
+              </button>
             </div>
           </div>
           <Feed />
